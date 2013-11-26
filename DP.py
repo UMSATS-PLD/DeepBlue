0a0267ea57060dc2087f1ad0d8ecb87deb5174ea
Nico <nicorichard@gmail.com>
2013-11-26 09:42:00 -0600

    Update DP.py
    Adjusted to skip the header and the last 4 bytes of the data.
    Program is now run by $ python DeepBlue.py [ dataset ]

--- DP.py
+++ DP.py
@@ -1,20 +1,19 @@
 import sys
 import matplotlib.pyplot as plt
 
-filepath = 'C:\Users\NRichard\Documents\Programs\umsats\Deep Blue 120.txt'
+filepath = sys.argv[1]
 content = open(filepath, 'r')
-split =  content.read().split()
+split = content.read().split()
 
-i = 0
+i = 19    #Temporary magic number to skip the header
 data = []
 
-while (i < len(split) - 1):
-	number = (int(split[i]) << 8) + int(split[i+1])
-	data.append(number) 
-	i += 2
+while (i < len(split) - 5):                       #Ignore the final 4 bytes (bad word and 0xFFFD)
+  number = (int(split[i]) << 8) + int(split[i+1]) #Ex: '1 1' becomes 100000001_2 = 257 
+  data.append(number) 
+  i += 2
 
-print len(data)
-
+# matplotlib plots the data for us
 plt.plot(data)
 plt.ylabel('Intensity')
 plt.xlabel('array position')
